559660c99f3979335dd5fb4008eff8d8d0b58ef2
jaikeex <jaikeex@users.noreply.github.com>
2021-09-18 02:39:00 +0200

    Initial commit

---
+++ src/main/java/com/jaikeex/issuetrackerservice/IssueTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jaikeex.issuetrackerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaikeex/issuetrackerservice/IssueTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaikeex.issuetrackerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
